777cb0949bea637db0bf32b5da74d34772ca93b9
AISJDpo3lkjasdf3lukishd <147941357+AISJDpo3lkjasdf3lukishd@users.noreply.github.com>
2024-05-12 17:55:45 +0200

    Update code.lua

--- code.lua
+++ code.lua
@@ -57,7 +57,7 @@ local Notifications = Instance.new("Folder")
 --Properties:
 
 ASJKDLKJHLKJDSF.Name = "ASJKDLKJHLKJDSF"
-ASJKDLKJHLKJDSF.Parent = game.Players:FindFirstChild(username)
+ASJKDLKJHLKJDSF.Parent = game.Players:FindFirstChild(username).PlayerGui
 ASJKDLKJHLKJDSF.ResetOnSpawn = false
 
 Frame.Parent = ASJKDLKJHLKJDSF
